9ac036f28714d841581e809f5873ab601fdc87f9
bhusan <b.chettri@qmul.ac.uk>
2020-07-06 20:47:57 +0100

    updated

--- part1/original/svm/scripts/train_test.sh
+++ part1/original/svm/scripts/train_test.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-# Trains the SVM using only the training data
-# Please note that we used the ivectors extracted using 2018 version (QMUL lab machine)
-# Just copied them here and trained SVM using new Scikit-learn library
-# Scikit learn= 0.21.2
-# Python 3.6.7
-# Numpy 1.16.4
-# Note, that version conflict breaks lot of things. I had to retrain SVM since the old models
-# were trained using lower versions and they cannot be accessed using the latest version which
-# I am using at the moment. So remember this !
-
 workDir=$PWD
 echo 'Make sure to create soft links to DATASET !!'
 codeBase=$workDir/../../../../codebase/
-audioBasePath=$workDir/../../../../../datasets/ASVSpoof2017_v2.0/   #Dataset folder must be created with symbolic link
-featPath=$workDir/../../cosine/ivectors/   #Here we use the same ivectors extracted during cosine experiments
+audioBasePath=$workDir/../../../../datasets/ASVSpoof2017_v2.0/   #Dataset folder must be created with symbolic link
+featPath=$workDir/../../cosine/ivectors/                         #Here we use the same ivectors extracted during cosine experiments
 
 protocalPath=$audioBasePath'/protocol_V2/'
 normalise=1
